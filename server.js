eeba3f490fe87d238428fe953090df01731931eb
spencercap <cappiello.spencer@gmail.com>
2017-10-27 17:41:25 -0400

    initial

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.send('this is the express test...');
+  console.log('working');
+});
+
+app.listen(process.env.PORT || 3000);
