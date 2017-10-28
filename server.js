b8dcd1044f61101349b408496ff31142ecc1894e
spencercap <cappiello.spencer@gmail.com>
2017-10-27 21:16:59 -0400

    another heroku fix2

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ server.listen(PORT, function() {
   console.log('server running on ' + PORT);
 }); // start the server
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+
 
 // routes
 app.get('/', function (req, res) {
